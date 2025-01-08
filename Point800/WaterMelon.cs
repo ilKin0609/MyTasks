4c41c04f050c9ba98358d1c7d44c74613c189836
ilKin0609 <ilkinmammadov.11@icloud.com>
2025-01-08 18:11:20 +0400

    CF project commit

---
+++ Point800/WaterMelon.cs
@@ -0,0 +1,13 @@
+int n = int.Parse(Console.ReadLine());
+if (n == 2)
+{
+    Console.WriteLine("NO");
+}
+else if (n % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("NO");
+}
